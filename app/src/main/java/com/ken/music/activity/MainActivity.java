58114ce23d9b5d16ea46b383a55f71380d898421
thien_tran <thien_tran@trams.co.kr>
2015-05-20 10:54:16 +0700

    just a test

--- app/src/main/java/com/ken/music/activity/MainActivity.java
+++ app/src/main/java/com/ken/music/activity/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // some change
     }
 
 
